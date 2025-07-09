bc1e6836d704ce7e08cab0afdd354e358d6428b3
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2025-07-09 02:05:07 -0300

    init: Arquivos de inicialização do Spring Boot

---
+++ src/test/java/com/guilherme/microsservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.microsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
