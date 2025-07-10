738ddc6b8d22cd2da40df04d6b68c0c0cc20a625
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2025-07-10 01:49:43 -0300

    feat(repository): cria UserRepository com métodos para busca por CPF e nome

---
+++ src/main/java/com/guilherme/microsservices/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.guilherme.microsservices.repository;
+
+import com.guilherme.microsservices.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    User findByCpf(String cpf);
+
+    List<User> querybyNomeLike(String name);
+
+
+}
