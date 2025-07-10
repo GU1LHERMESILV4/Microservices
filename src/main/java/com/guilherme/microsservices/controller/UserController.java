6661ab77e43b39f90ff623da69f594a6b0b898ce
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2025-07-10 00:08:29 -0300

    feat(controller): adiciona endpoints GET/{cpf}, POST e DELETE ao UserController

--- src/main/java/com/guilherme/microsservices/controller/UserController.java
+++ src/main/java/com/guilherme/microsservices/controller/UserController.java
@@ -2,11 +2,11 @@ package com.guilherme.microsservices.controller;
 
 import com.guilherme.microsservices.dto.UserDTO;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,6 +51,28 @@ public class UserController {
     @GetMapping
     public List<UserDTO> getUsers() {
         return usuarios;
+    }
+
+    @GetMapping("/{cpf}")
+    public UserDTO getUsersFiltro(@PathVariable String cpf) {
+        return usuarios.stream()
+                .filter(userDTO -> userDTO.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("User not found."));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO inserir(@RequestBody @Valid UserDTO userDTO) {
+        userDTO.setDataCadastro(LocalDateTime.now());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean remover(@PathVariable String cpf) {
+        return usuarios
+                .removeIf(userDTO -> userDTO.getCpf().equals(cpf));
     }
 
     @GetMapping("/")
