ad0298c8b531a337ca6a089b1e52cc6b45a70d5e
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2025-07-10 00:06:04 -0300

    feat: adiciona validações obrigatórias no UserDTO com @NotBlank

--- src/main/java/com/guilherme/microsservices/dto/UserDTO.java
+++ src/main/java/com/guilherme/microsservices/dto/UserDTO.java
@@ -6,18 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+    @NotBlank(message = "CPF é obrigatório")
     private String cpf;
     private String endereco;
+    @NotBlank(message = "E-mail é obrigatório")
     private String email;
     private String telefone;
     private LocalDateTime dataCadastro;
+
 }
